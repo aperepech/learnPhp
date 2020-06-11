2175e1bd6df7214e4b3d60b9755ffdcfb91ec0a5
aperepech <aperepech@yandex.ru>
2020-06-11 16:34:53 +0300

    в Tag добавлен наследник ul/ol

--- tag.php
+++ tag.php
@@ -229,5 +229,45 @@ interface iTag
 
    }
 
-  
- 
+   	class ListItem extends Tag
+	{
+		public function __construct()
+		{
+			// Вызываем конструктор родителя, передав в качестви имени 'li':
+			parent::__construct('li');
+		}
+	}
+
+	class HtmlList extends Tag
+	{
+		private $items = [];
+		
+		public function addItem(ListItem $li)
+		{
+			$this->items[] = $li;
+			return $this;
+		}
+		
+		public function show()
+		{
+			$result = $this->open();
+			
+			foreach ($this->items as $item) {
+				$result .= $item->show();
+			}
+			
+			$result .= $this->close();
+			
+			return $result;
+		}
+
+	}
+
+   /* $list = new HtmlList('ul');
+
+    echo $list->setAttr('class', 'eee')
+		->addItem((new ListItem())->setText('item1')->setAttr('class', 'first'))
+		->addItem((new ListItem())->setText('item2'))
+		->addItem((new ListItem())->setText('item3'))
+		->show();*/
+   
